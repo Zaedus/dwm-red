37c6caa35e5619497707064662e5074b9cf13ea6
Zaedus <nintendozaedus@gmail.com>
2021-03-30 11:42:22 -0500

    :art: Changed workspace icons

--- config.h
+++ config.h
@@ -48,7 +48,7 @@ static const unsigned int alphas[][3] = {
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "ﴬ", "", "", "", "", "" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
